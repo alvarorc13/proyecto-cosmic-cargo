d345b5626ca95c850a75879a887971d785f91bb0
javierroldan2207 <javierroldan2207@gmail.com>
2025-12-15 09:53:29 +0100

    Creacion de boton y carta , falta mejorarlo

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import './App.css'
 import Router  from './Router'
+import CharacterCard from './components/CharacterCard'
+import Button from './components/Button'
 
 
 
