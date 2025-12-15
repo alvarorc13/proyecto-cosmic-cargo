c811141cbcb3d1609dfa02c3662c18e50cffcb12
Jesús <jes9mimi9@gmail.com>
2025-12-15 09:44:18 +0100

    Añadida las Locations a la api

--- src/services/rickAndMortyService.tsx
+++ src/services/rickAndMortyService.tsx
@@ -1,4 +1,5 @@
 const urlCharacter: string = "https://rickandmortyapi.com/api/character";
+const urlLocation: string = "https://rickandmortyapi.com/api/location";
 
 export async function getFirst20Characters(): Promise</*Character*/any> {
     try {
@@ -25,6 +26,39 @@ export async function get20CharactersFromPage(page: number): Promise</*Character
 export async function getCharacterById(id: number): Promise</*Character*/any> {
     try {
         return await fetch(urlCharacter + "/" + id, { method: "GET" })
+            .then(response => response.ok? response.json(): Promise.reject(response))
+            .catch(error => console.error(error));
+    } catch (error) {
+        console.error(error);
+        return new Promise(() => {});
+    }
+}
+
+export async function getFirst6Locations(): Promise</*Location*/any> {
+    try {
+        return await fetch(urlLocation, { method: "GET" })
+            .then(response => response.ok? response.json(): Promise.reject(response))
+            .catch(error => console.error(error));
+    } catch (error) {
+        console.error(error);
+        return new Promise(() => {});
+    }
+}
+
+export async function get6LocationsFromPage(page: number): Promise</*Location*/any> {
+    try {
+        return await fetch(urlLocation + "?page=" + page, { method: "GET" })
+            .then(response => response.ok? response.json(): Promise.reject(response))
+            .catch(error => console.error(error));
+    } catch (error) {
+        console.error(error);
+        return new Promise(() => {});
+    }
+}
+
+export async function getLocationById(id: number): Promise</*Location*/any> {
+    try {
+        return await fetch(urlLocation + "/" + id, { method: "GET" })
             .then(response => response.ok? response.json(): Promise.reject(response))
             .catch(error => console.error(error));
     } catch (error) {
