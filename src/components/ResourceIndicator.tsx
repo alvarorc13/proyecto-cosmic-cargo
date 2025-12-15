a3d65e67a51dac53508da8bafbcbe86ef3ec2598
javierroldan2207 <javierroldan2207@gmail.com>
2025-12-15 14:14:57 +0100

    Realizacion de boton y ResourceIndicators

--- src/components/ResourceIndicator.tsx
+++ src/components/ResourceIndicator.tsx
@@ -1,12 +1,21 @@
-import { useContext } from "react"
+import React from 'react';
 
-const ResourceIndicatos = () => {
-    
-    return (
-        <>
-            
-        </>        
-    )
+interface ResourceBadgeProps {
+    icon:string,
+    fuel:number,
+    credit:number
 }
+const ResourceBadge = ({ icon, fuel, credit }:ResourceBadgeProps) => {
+  return (
+    <div className="flex items-center bg-gray-800 text-white px-3 py-1 rounded-full space-x-2 shadow-md">
+      {/* Icono */}
+      <span className="text-xl">{icon}</span>
+      {/* Gasolina */}
+      <span className="font-medium">{fuel}:</span>
+      {/* Monedas*/}
+      <span className="font-bold">{credit}</span>
+    </div>
+  );
+};
 
-export default ResourceIndicatos
+export default ResourceBadge;
