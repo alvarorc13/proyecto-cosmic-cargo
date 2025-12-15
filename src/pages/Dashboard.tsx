15aba1abfaf31a380dd93a09c785aea9f2d69b22
daniguti99 <dgm1021999@gmail.com>
2025-12-15 13:48:31 +0100

    Subida de realización del paso 8: Puente de mando

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,7 +1,40 @@
+import { useContext } from "react"
+import { GlobalContext } from "../context/ShipContext"
+import { type Character } from '../types/character';
+import CharacterCard from '../components/CharacterCard';
+
+
 export default function Dashboard() {
+
+    const context = useContext(GlobalContext);
+    
+    if(!context) return null;
+
+    const { fuel, characters } = context;
+
     return(
         <>
-            <h1>Hola Dashboard</h1>
+            <div className="container mt-4">
+                <h1 className="mb-4">🚀 Puente de Mando</h1>
+
+                {fuel <= 0 && (
+                    <div className="alert alert-danger text-center" role="alert">
+                        🚨 Game Over: ¡La nave se ha quedado sin combustible!
+                    </div>
+                )}
+
+                {characters.length == 0 ? (
+                    <div className="alert alert-danger text-center" role="alert">
+                        🚨 ¡La nave no tiene tripulantes! Contrátalos en la Cantina.
+                    </div>
+                ) : (
+                    characters.map((character: Character) => (
+                        <CharacterCard character={character} />
+                    ))
+                )}
+                    
+            </div>
+
         </>
     )
 }
