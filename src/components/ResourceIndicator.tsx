604fbc529569e794e0e5d79114223f051ecba927
javierroldan2207 <javierroldan2207@gmail.com>
2025-12-16 12:28:31 +0100

    Estilos de resourceBadge

--- src/components/ResourceIndicator.tsx
+++ src/components/ResourceIndicator.tsx
@@ -7,23 +7,15 @@ interface ResourceBadgeProps {
 
 const ResourceBadge = ({ icon, fuel, credit }: ResourceBadgeProps) => {
   return (
-    <div className="flex items-center bg-black/80 text-white p-3 rounded-xl border border-cyan-500 shadow-lg space-x-4 w-72">
+    <div className="resourceBadge">
       <span className="text-3xl">
-        <img src={icon} alt="20" />
+        <img id="img" src={icon} alt="" />
         </span>
       
       <div className="flex-1 space-y-2">
-        
         <div className="text-[10px] flex justify-between"><span>CASH</span> <span>${credit}</span></div>
-        <div className="h-1.5 w-full bg-gray-700 rounded-full">
-          <div className="h-full bg-yellow-400 rounded-full" style={{ width: '100%' }}></div>
-        </div>
-
-       
         <div className="text-[10px] flex justify-between"><span>FUEL</span> <span>{fuel}%</span></div>
-        <div className="h-1.5 w-full bg-gray-700 rounded-full">
-          <div className="h-full bg-cyan-400 rounded-full" style={{ width: `${fuel}%` }}></div>
-        </div>
+        
       </div>
     </div>
   );
