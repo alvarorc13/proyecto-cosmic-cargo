499f7e5abbf12bdf9d919127ed0842e697569e53
javierroldan2207 <javierroldan2207@gmail.com>
2025-12-16 12:02:24 +0100

    Edicion de estilos

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,17 @@
 import { Link, Outlet } from 'react-router-dom'
+import ResourceBadge from './ResourceIndicator'
+import { useContext } from 'react';
+import { GlobalContext } from '../context/ShipContext';
 
 export function Layout() {
+
+  const context = useContext(GlobalContext);
+      
+      if(!context) return null;
+  
+      const { fuel, credit } = context;
+
+      
   return (
     <>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
@@ -9,7 +20,7 @@ export function Layout() {
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
             <span className="navbar-toggler-icon"></span>
           </button>
-          
+          <ResourceBadge icon={"src/assets/Gemini_Generated_Image_nizm1gnizm1gnizm-removebg-preview.png"} fuel={fuel} credit={credit}/>
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav ms-auto">
               <li className="nav-item">
--- src/components/ResourceIndicator.tsx
+++ src/components/ResourceIndicator.tsx
@@ -1,19 +1,30 @@
-import React from 'react';
 
 interface ResourceBadgeProps {
-    icon:string,
-    fuel:number,
-    credit:number
+    icon: string;
+    fuel: number;
+    credit: number;
 }
-const ResourceBadge = ({ icon, fuel, credit }:ResourceBadgeProps) => {
+
+const ResourceBadge = ({ icon, fuel, credit }: ResourceBadgeProps) => {
   return (
-    <div className="flex items-center bg-gray-800 text-white px-3 py-1 rounded-full space-x-2 shadow-md">
-      {/* Icono */}
-      <span className="text-xl">{icon}</span>
-      {/* Gasolina */}
-      <span className="font-medium">{fuel}:</span>
-      {/* Monedas*/}
-      <span className="font-bold">{credit}</span>
+    <div className="flex items-center bg-black/80 text-white p-3 rounded-xl border border-cyan-500 shadow-lg space-x-4 w-72">
+      <span className="text-3xl">
+        <img src={icon} alt="20" />
+        </span>
+      
+      <div className="flex-1 space-y-2">
+        
+        <div className="text-[10px] flex justify-between"><span>CASH</span> <span>${credit}</span></div>
+        <div className="h-1.5 w-full bg-gray-700 rounded-full">
+          <div className="h-full bg-yellow-400 rounded-full" style={{ width: '100%' }}></div>
+        </div>
+
+       
+        <div className="text-[10px] flex justify-between"><span>FUEL</span> <span>{fuel}%</span></div>
+        <div className="h-1.5 w-full bg-gray-700 rounded-full">
+          <div className="h-full bg-cyan-400 rounded-full" style={{ width: `${fuel}%` }}></div>
+        </div>
+      </div>
     </div>
   );
 };
